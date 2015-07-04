1d3b04ab9b1f43f07f90411392ef45bad9e7a405
Grant J. Butler <butler.grant.j@gmail.com>
2015-07-04 13:27:10 -0400

    Add TAGFile.

---
+++ TagKit/Classes/TAGFile.h
@@ -0,0 +1,13 @@
+//
+//  TAGFile.h
+//  TagKit
+//
+//  Created by Grant Butler on 7/4/15.
+//  Copyright © 2015 Grant J. Butler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TAGFile : NSObject
+
+@end
---
+++ TagKit/Classes/TAGFile.m
@@ -0,0 +1,13 @@
+//
+//  TAGFile.m
+//  TagKit
+//
+//  Created by Grant Butler on 7/4/15.
+//  Copyright © 2015 Grant J. Butler. All rights reserved.
+//
+
+#import "TAGFile.h"
+
+@implementation TAGFile
+
+@end
