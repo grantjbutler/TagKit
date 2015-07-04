dc8b40cdeb056c4cef6a1162e286d2068914b15d
Grant J. Butler <butler.grant.j@gmail.com>
2015-07-04 16:02:07 -0400

    Fix imports again.

--- TagKit/Classes/NSString+TAGTaglibSupport.h
+++ TagKit/Classes/NSString+TAGTaglibSupport.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "TagKitDefines.h"
+#import <TagKit/TagKitDefines.h>
 
 #include "tstring.h"
 
--- TagKit/Classes/TAGFile.h
+++ TagKit/Classes/TAGFile.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <TagKit/TagKitDefines.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
