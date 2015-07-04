f80525c8d5d10b3b5ad111222c5138270a724e17
Grant J. Butler <butler.grant.j@gmail.com>
2015-07-04 15:01:50 -0400

    Add unit tests for initializers.

---
+++ TagKitTests/TAGFileTests.m
@@ -0,0 +1,38 @@
+//
+//  TAGFileTests.m
+//  TagKit
+//
+//  Created by Grant Butler on 7/4/15.
+//  Copyright © 2015 Grant J. Butler. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <TagKit/TagKit.h>
+
+@interface TAGFileTests : XCTestCase
+
+@end
+
+@implementation TAGFileTests
+
+- (void)testInitWithPathInitializer {
+	NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+	NSString *path = [bundle pathForResource:@"TestAudioFile" ofType:@"wav"];
+	TAGFile *file = [[TAGFile alloc] initWithPath:path];
+	
+	XCTAssertNotNil(file);
+}
+
+- (void)testInitWithFilePathInitializer {
+	NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+	NSURL *URL = [bundle URLForResource:@"TestAudioFile" withExtension:@"wav"];
+	TAGFile *file = [[TAGFile alloc] initWithFileURL:URL];
+	
+	XCTAssertNotNil(file);
+}
+
+- (void)testInvalidFileInitializer {
+	
+}
+
+@end
