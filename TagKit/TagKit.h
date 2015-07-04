0b0c7013fa7db8e81e6d08b2691c60d0977126bc
Grant J. Butler <butler.grant.j@gmail.com>
2015-07-04 12:21:43 -0400

    Initial Commit

---
+++ TagKit/TagKit.h
@@ -0,0 +1,19 @@
+//
+//  TagKit.h
+//  TagKit
+//
+//  Created by Grant Butler on 7/4/15.
+//  Copyright © 2015 Grant J. Butler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TagKit.
+FOUNDATION_EXPORT double TagKitVersionNumber;
+
+//! Project version string for TagKit.
+FOUNDATION_EXPORT const unsigned char TagKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TagKit/PublicHeader.h>
+
+
