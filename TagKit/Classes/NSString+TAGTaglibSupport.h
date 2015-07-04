458654d867b785faa27b203b0c6e1b95f718498c
Grant J. Butler <butler.grant.j@gmail.com>
2015-07-04 15:50:30 -0400

    Add NSString category to support TagLib::String.

---
+++ TagKit/Classes/NSString+TAGTaglibSupport.h
@@ -0,0 +1,13 @@
+//
+//  NSString+TAGTaglibSupport.h
+//  TagKit
+//
+//  Created by Grant Butler on 7/4/15.
+//  Copyright © 2015 Grant J. Butler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (TAGTaglibSupport)
+
+@end
---
+++ TagKit/Classes/NSString+TAGTaglibSupport.mm
@@ -0,0 +1,13 @@
+//
+//  NSString+TAGTaglibSupport.m
+//  TagKit
+//
+//  Created by Grant Butler on 7/4/15.
+//  Copyright © 2015 Grant J. Butler. All rights reserved.
+//
+
+#import "NSString+TAGTaglibSupport.h"
+
+@implementation NSString (TAGTaglibSupport)
+
+@end
